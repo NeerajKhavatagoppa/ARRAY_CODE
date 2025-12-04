c95ebc85fb0efe0f9e47ed15e4db461a77772815
Neeraj <156562108+NeerajKhavatagoppa@users.noreply.github.com>
2025-12-04 22:42:20 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements in array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in-place
+    for (int i = 0; i < n/2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
 
